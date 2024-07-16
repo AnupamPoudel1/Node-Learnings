55d2817a463f8153cc89491b2dcd8fef87f3e52e
Anupam <anupampoudel78@gmail.com>
2024-07-16 23:05:33 +0545

    common knowledge about node

---
+++ aboutNPM.js
@@ -0,0 +1,10 @@
+// npm : node package manager, global command that comed with node.
+// npm --version
+
+// local dependency : using installed dependency only in this particular project
+// npm install <packageName> or npm i <packageName>
+// eg: npm install nodemon or npm i nodemon
+
+// global dependency : using installed dependency in any project
+// npm install -g <packageName> or npm i -g <packageName>
+// eg: npm install -g nodemon or npm i -g nodemon
