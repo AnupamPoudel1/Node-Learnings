7282bed541e7fdb5fc4b0d2476a63818b6f3fe1a
Anupam <anupampoudel78@gmail.com>
2024-07-17 11:05:27 +0545

    initializing package json and installing dependencies

--- aboutNPM.js
+++ aboutNPM.js
@@ -8,3 +8,11 @@
 // global dependency : using installed dependency in any project
 // npm install -g <packageName> or npm i -g <packageName>
 // eg: npm install -g nodemon or npm i -g nodemon
+
+// package.json : manifest file (stores important info about project/package)
+// can be created in 3 ways as follow:
+// 1) manual approach (create package.json in the root file and create properties etc)
+// 2) npm init (step by step, press enter to skip)
+// 3) npm init -y (ecerything default)
+
+// package.json is used to store used dependencies.
