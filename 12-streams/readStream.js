491d0aa27f937d144f734dc03c992f36dccdf150
Anupam <anupampoudel78@gmail.com>
2024-07-23 21:51:44 +0545

    Additional information about createReadStream

--- 12-streams/readStream.js
+++ 12-streams/readStream.js
@@ -1,11 +1,25 @@
-const {createReadStream} = require('fs');
+const { createReadStream } = require('fs');
 
 const stream = createReadStream('./content/big.txt');
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000, encoding: 'utf-8' });
+
+// Default size - 65kb
+// Last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 });
+// const stream = createReadStream('./content/big.txt', { encoding: 'utf-8' });
+
 
 stream.on('data', (result) => {
-  console.log(result);  
+    console.log(result);
 })
 
 // As we can see in output
-// Instead of reading all the data in the file 
+// Instead of reading all the data in the file
 // The readStream reads it in chunks
+
+// There is also an error event
+// Example:
+stream.on('error', (err) => {
+    console.log(err);
+})
