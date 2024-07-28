8302616bbb236442fe378e54206c53b4efa10a1e
Anupam <anupampoudel78@gmail.com>
2024-07-28 16:26:53 +0545

    HTTP Headers Basics

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const http = require('http');
 
+// The content type in header matters
+// We can see in the following example that:
+// We are sending response as html but the content type is text/plain.
+// So when the response displays in browser we will get: <h1>Home Page</h1> as output.
+// But if we will keep content type as text/html then the output will he Home Page in heading 1.
 const server = http.createServer((req, res) => {
-    console.log('User hit the server');
-    res.end('Home Page');
+    res.writeHead(
+        200, 
+        // {'content-type':'text/html'},
+        {'content-type':'text/plain'}
+    );
+    res.write('<h1>Home Page</h1>');
+    res.end();
 });
-
-// Every response must end with .end to signify browser that the response is done.
 
 server.listen(3000);
