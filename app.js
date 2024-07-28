52f78f6ae8bb89329f3377795f076602ccd4b412
Anupam <anupampoudel78@gmail.com>
2024-07-28 16:10:44 +0545

    HTTP basics

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 const http = require('http');
 
-// Creating a server without using EventEmitter - HTTP module
-// const server = http.createServer((req, res) => {
-//     res.end('Welcome');
-// });
+const server = http.createServer((req, res) => {
+    console.log('User hit the server');
+    res.end('Home Page');
+});
 
-// Using EventEmitter API
-const server = http.createServer();
+// Every response must end with .end to signify browser that the response is done.
 
-// emits request event
-// subscribe to it/ listen to it/ respond to it
-server.on('request', (req, res) => {
-    res.end('Welcome To Our Page');
-})
-
-server.listen(3500);
-
-// Likewise there are several events in http that we can check in the documentation of the node.js
-// We need to explore that on our own
+server.listen(3000);
